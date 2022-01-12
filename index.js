a50c56ddaef70373635d804276219f5e53250f7f
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2022-01-11 22:15:57 -0300

    [reformulado o código]

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({
   	headless: true
   });
+  //abre o navegador
   const page = await browser.newPage();
   await page.goto('https://www.correios.com.br/', { 
   	timeout: 60000
   });
+  //digita no buscar pelo cep o numero do cep e aperta enter
   await page.type('input#relaxation', '07909050');
   await page.keyboard.press('Enter');
 
@@ -15,14 +17,14 @@ const puppeteer = require('puppeteer');
 
   const abas = await (await browser.pages());
   const aba3 = abas[2];
-
+  //pega no site o elemento DOM da tabela do endereço
   	function getTable() {
   		const table = document.querySelector("#resultado-DNEC");
   		return table.innerText;
   	}
 
 	const dadosTable = await aba3.evaluate(getTable);
-
+  //mostra no console do node os dados do endereço
 	console.log(dadosTable);
 
   await browser.close();
