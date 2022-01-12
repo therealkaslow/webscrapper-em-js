572134a5eabd918ac63146c8f1b1230be54a8213
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2022-01-11 22:09:34 -0300

    [reformulado o código]

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+  	headless: true
+  });
   const page = await browser.newPage();
-  //inserir o cep na linha abaixo!
-  const cepCorreios = /* Do lado => */ "07909050";
-  //entra no site dos correios
-  await page.goto(`https://buscacepinter.correios.com.br/app/endereco/`);
-  await page.waitFor(100);
-
-  //digita o CEP
-  await page.type('input[name="endereco"]', cepCorreios, {
-    delay: 100,
+  await page.goto('https://www.correios.com.br/', { 
+  	timeout: 60000
   });
+  await page.type('input#relaxation', '07909050');
+  await page.keyboard.press('Enter');
 
-  // clica no botão de buscar
-  await page.click('button[class="primario"]');
+  await page.waitForTimeout(5000);
 
-  //pega os dados da tabela do site e joga no terminal node.js
-  const endCorreios = await page.evaluate(() => {
-    return document.querySelector('th[data-th="Logradouro/Nome"]');   
-  });
-  console.log(`O Endereço do CEP ${cepCorreios} é: ${endCorreios}`)
+  const abas = await (await browser.pages());
+  const aba3 = abas[2];
+
+  	function getTable() {
+  		const table = document.querySelector("#resultado-DNEC");
+  		return table.innerText;
+  	}
+
+	const dadosTable = await aba3.evaluate(getTable);
+
+	console.log(dadosTable);
+
   await browser.close();
 })();
